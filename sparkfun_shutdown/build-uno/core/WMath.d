891e31f7b49d1e5eccdde679b1b868ced94a32e5
Ali Saghiran <a.saghiran@gmail.com>
2016-10-10 03:06:10 +0200

    deleting build folder (keeping it local is better)

--- sparkfun_shutdown/build-uno/core/WMath.d
+++
@@ -1,2 +0,0 @@
-build-uno/core/WMath.o: \
- /usr/share/arduino/hardware/arduino/cores/arduino/WMath.cpp
--- sparkfun_shutdown/build-uno/mcp2515.d
+++
@@ -1,5 +0,0 @@
-build-uno/mcp2515.o: mcp2515.c \
- /usr/share/arduino/hardware/arduino/cores/arduino/Arduino.h \
- /usr/share/arduino/hardware/arduino/cores/arduino/binary.h \
- /usr/share/arduino/hardware/arduino/variants/standard/pins_arduino.h \
- global.h mcp2515.h mcp2515_defs.h defaults.h
